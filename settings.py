62c39c55ab8e066ee38402c0fe08f80cb6ab7698
Feuer Wasser <ruess82@gmail.com>
2024-09-03 13:05:03 -0400

    updating settings page GUI

--- settings.py
+++ settings.py
@@ -1,8 +1,10 @@
 from kivy.uix.actionbar import BoxLayout
 from kivy.app import App
 
+class SettingsScreen(BoxLayout):
+    pass
 class SettingsApp(App):
     def build(self):
-        return BoxLayout()
+        return SettingsScreen()
 if __name__=='__main__':
     SettingsApp().run()
