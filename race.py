98d0e103d53e43766aad655756ecf02d2cbfe4ae
Feuer Wasser <ruess82@gmail.com>
2024-09-05 13:13:08 -0400

    refactoring layouts with custom class rules. update all .py files to accept the new kivy classes.

--- race.py
+++ race.py
@@ -1,16 +1,42 @@
+from kivy.config import Config
+Config.set('graphics', 'width', '1200')
+Config.set('graphics', 'height', '675') 
+Config.set('graphics', 'resizable', '0')
+
 
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
-from kivy.config import Config
 
-Config.set('graphics', 'width', '1200')
-Config.set('graphics', 'height', '675')
-Config.set('graphics', 'resizable', '0')
+# We're going to need the RelativeLayout class.
+from kivy.uix.relativelayout import RelativeLayout
+
+from kivy.properties import NumericProperty, StringProperty
+
+class SlugStats(BoxLayout):
+    name = StringProperty('')
+    wins = NumericProperty(0)
+    win_percent = NumericProperty(0)
+
+class PlayerStats(BoxLayout):
+    name = StringProperty('')
+    money = NumericProperty(0)
+
+class SlugInfo(BoxLayout):
+    y_position = NumericProperty(0)
+    name = StringProperty('')
+    wins = NumericProperty(0)
+
+class SlugImage(RelativeLayout):
+    body_image = StringProperty('')
+    eye_image = StringProperty('')
+    y_position = NumericProperty(0)
+
 class RaceScreen(BoxLayout):
     pass
+
 class RaceApp(App):
     def build(self):
         return RaceScreen()
 
-if __name__=='__main__':
+if __name__ == '__main__':
     RaceApp().run()
--- settings.py
+++ settings.py
@@ -1,13 +1,22 @@
-from kivy.uix.boxlayout import BoxLayout
-from kivy.app import App
 from kivy.config import Config
-
 Config.set('graphics', 'width', '1200')
-Config.set('graphics', 'height', '675')
+Config.set('graphics', 'height', '675') 
 Config.set('graphics', 'resizable', '0')
+
+
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import StringProperty
+
+class PlayerCount(BoxLayout):
+    count_text = StringProperty('')
+
+class PlayerSettings(BoxLayout):
+    label_text = StringProperty('')
 
 class SettingsScreen(BoxLayout):
     pass
+
 class SettingsApp(App):
     def build(self):
         return SettingsScreen()
